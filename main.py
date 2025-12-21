c857d51aacf3a722a3c1718bd9865f0c89c0556f
alexelte5 <alexeltekow@gmail.com>
2025-12-21 22:18:20 +0100

    test_building adjusted for visualization purposes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from layered_a_star_ChatGPT import benchmark_all_pairs, layered_a_star, load_building
+import os
 
 if __name__ == '__main__':
+    print(os.getcwd())
     graph, model = load_building("test_building.json")
 
     # Example search
-    START = "entrance_F0"
-    GOAL = "OFFICE_F1"
+    START = "entrance_L0"
+    GOAL = "room_L2_1"
 
     path, cost, dt = layered_a_star(graph, model, START, GOAL, visualize=True)
 
